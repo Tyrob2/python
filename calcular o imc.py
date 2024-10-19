fddde84a140853a62708ac56a6ea6ffaf17768e4
Caio Butkousky <117804075+Tyrob2@users.noreply.github.com>
2024-10-18 22:47:00 -0300

    Update calcular o imc.py

--- calcular o imc.py
+++ calcular o imc.py
@@ -1,20 +1,23 @@
-#Função para calcular o IMC
+# Função para calcular o IMC
 def calcular_imc(peso, altura):
-    return peso / (altura **2)
-#Função para classificar o IMC baseado no sexo
+    return peso / (altura ** 2)
+
+# Função para classificar o IMC baseado no sexo
 def classificar_imc(imc, sexo):
     if sexo.lower() == 'm':
-        #Classificação Masculina
+        # Classificação Masculina
         if imc < 20.7:
             return 'Abaixo do peso'
         elif 20.7 <= imc <= 26.4:
-            return 'peso normal'
+            return 'Peso normal'
         elif 26.4 < imc <= 27.8:
             return 'Levemente acima do peso'
         elif 27.8 < imc <= 31.1:
             return 'Acima do peso ideal (sobrepeso)'
+        else:
+            return 'Obesidade'
     elif sexo.lower() == 'f':
-        #Classificação Feminina
+        # Classificação Feminina
         if imc < 19.1:
             return 'Abaixo do peso'
         elif 19.1 <= imc <= 25.8:
@@ -22,31 +25,32 @@ def classificar_imc(imc, sexo):
         elif 25.8 < imc <= 27.3:
             return 'Levemente acima do peso'
         elif 27.3 < imc <= 32.3:
-            return ' Acima do peso ideal (sobrepeso)'
+            return 'Acima do peso ideal (sobrepeso)'
         else:
             return 'Obesidade'
     else:
         return "Sexo inválido! Use 'M' para masculino ou 'F' para feminino"
 
-#Função principal para coletar dados e calcular o IMC
+# Função principal para coletar dados e calcular o IMC
 def main():
     try:
         peso = float(input('Digite o peso em kg: '))
         altura = float(input('Digite a altura em metros: '))
-        sexo =input('Digite o sexo (M para masculino, e F para feminino): ')
+        sexo = input('Digite o sexo (M para masculino, e F para feminino): ')
 
-        #Calcular o IMC
+        # Calcular o IMC
         imc = calcular_imc(peso, altura)
         print(f'Seu IMC é: {imc:.2f}')
 
-        #Classificar o IMC baseado sexo
+        # Classificar o IMC baseado no sexo
         classificacao = classificar_imc(imc, sexo)
         print(f'Classificação: {classificacao}')
 
     except ValueError:
         print('Por favor, insira valores numéricos válidos para peso e altura.')
-        return main()
+        main()
 
-#Executa o programa
-main()
-        
+# Executa o programa
+if __name__ == "__main__":
+    main()
+    input("\nPressione qualquer tecla para sair...")
